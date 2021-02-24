8ae41fdb7691eb3e4afa0f02334cae8015c6775a
irinazay <irinavzaytseva@gmail.com>
2021-02-24 16:34:33 -0500

    Flask, Flask-SQLAlchemy, Flask-WTF  project

---
+++ forms.py
@@ -0,0 +1,38 @@
+"""Forms for adopt app."""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SelectField, TextAreaField, BooleanField
+from wtforms.validators import InputRequired, NumberRange, URL, Optional
+
+
+class AddPetForm(FlaskForm):
+    """Form for adding pets."""
+
+    name = StringField("Pet Name", validators=[InputRequired()])
+
+    species = SelectField("Species", choices=[
+                          ("cat", "Cat"), ("dog", "Dog"), ("porcupine", "Porcupine")])
+
+    photo_url = StringField("Photo URL", validators=[Optional(), URL()])
+
+    age = IntegerField("Age", validators=[
+                       Optional(), NumberRange(min=0, max=30)])
+
+    notes = TextAreaField("Comments", validators=[Optional()])
+
+
+class EditPetForm(FlaskForm):
+    """Form for editing an existing pet."""
+
+    photo_url = StringField(
+        "Photo URL",
+        validators=[Optional(), URL()],
+    )
+
+    notes = TextAreaField(
+        "Comments",
+        validators=[Optional()],
+    )
+
+    available = BooleanField("Available?")
+
